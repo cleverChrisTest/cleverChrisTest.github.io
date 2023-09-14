d7f698a8ccd57adf4d609301b5034203956589a6
Christian <82599146+cleverChrisTest@users.noreply.github.com>
2023-09-14 10:56:43 +0100

    Update files

--- js/index.js
+++ js/index.js
@@ -1,42 +1,22 @@
 import { fecthUrl } from './funciones.js';
 import { createCardGroup } from './cardGenerator.js';
 import { scrollEffect, scrollUpButtonEffect } from './scrollEffect.js';
+import { arr, languages } from './info.js';
 
-function loadKnowledgeSection(){
-    let url = "https://raw.githubusercontent.com/cleverChrisTest/cleverChrisTest.github.io/main/php/info.json";
+function loadKnowledgeSection() {
+
     let infoTable = document.getElementById("infoTable");
 
-    fecthUrl(url, (data) => {
-        let table = createCardGroup(data);
-        table.classList.add("table", "table-dark");
-        infoTable.appendChild(table);
-    });
+    let table = createCardGroup(languages);
+    table.classList.add("table", "table-dark");
+    infoTable.appendChild(table);
 }
 
 function init() {
-    let img1 = "https://images.pexels.com/photos/3888151/pexels-photo-3888151.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
-    let img2 = "https://images.pexels.com/photos/1181263/pexels-photo-1181263.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
 
-    let arr = [
-        {
-            title: "Próxima posición", 
-            img: [img1, "Nombre de tu empresa"], 
-            description: "En esta sección irá el nombre de tu empresa, con un link a tu web y una breve descripción de las tareas desempeñadas.", 
-            button: ["www.google.es", "Link a la web"], 
-            footer_text: "text" 
-        },
-        {
-            title: "Próxima posición", 
-            img: [img2, "Nombre de la siguiente empresa"], 
-            description: "En esta sección irá la posición que ocupe en tu empresa", 
-            button: ["www.google.es", "Link a la web"], 
-            footer_text: "text" 
-        }
-
-    ];
 
     loadKnowledgeSection();
-    
+
     tarjetas.append(createCardGroup(arr));
     document.querySelectorAll("section")[0].classList.add("mostrar");
 
---
+++ js/info.js
@@ -0,0 +1,59 @@
+let img1 = "https://images.pexels.com/photos/3888151/pexels-photo-3888151.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+let img2 = "https://images.pexels.com/photos/1181263/pexels-photo-1181263.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+
+export let arr = [
+    {
+        title: "Próxima posición", 
+        img: [img1, "Nombre de tu empresa"], 
+        description: "En esta sección irá el nombre de tu empresa, con un link a tu web y una breve descripción de las tareas desempeñadas.", 
+        button: ["www.google.es", "Link a la web"], 
+        footer_text: "text" 
+    },
+    {
+        title: "Próxima posición", 
+        img: [img2, "Nombre de la siguiente empresa"], 
+        description: "En esta sección irá la posición que ocupe en tu empresa", 
+        button: ["www.google.es", "Link a la web"], 
+        footer_text: "text" 
+    }
+
+];
+
+export let languages = [
+    {
+        title: "HTML",
+        img: "['https://raw.githubusercontent.com/cleverChrisTest/cleverChrisTest.github.io/main/media/html.png', 'html logo'",
+        description: "2 años"
+    },
+    {
+        title: "CSS",
+        description: "2 año"
+    },
+    {
+        title: "Javascript",
+        img: "['https://raw.githubusercontent.com/cleverChrisTest/cleverChrisTest.github.io/main/media/javascript.png', 'javascript logo']",
+        description: "1 año"
+    },
+    {
+        title: "Java",
+        img: "['https://raw.githubusercontent.com/cleverChrisTest/cleverChrisTest.github.io/main/media/Sin%20t%C3%ADtulo.png', '']",
+        description: "Clases, objetos, herencia, tipos de datos, genéricos y paquetes. Excepciones, entrada/salida básica, concurrencia, expresiones regulares y el entorno de la plataforma. Expersiones lambda."
+    },
+    {
+        title: "Spring Framework",
+        description: "1 mes"
+    },
+    {
+        title: "Spring Data",
+        description: "1 mes"
+    },
+    {
+        title: "Spring Security",
+        description: "1 mes"
+    },
+    {
+        title: "Vue.js",
+        description: "Conceptos básicos de Vuejs."
+    }
+
+];
