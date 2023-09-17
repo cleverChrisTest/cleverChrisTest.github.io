1d7ddfad2a31d5dd87cab825d1a898467361951e
Christian <82599146+cleverChrisTest@users.noreply.github.com>
2023-09-17 13:12:06 +0100

    Feat: added cookie consent banner

---
+++ js/banner.js
@@ -0,0 +1,7 @@
+decline.addEventListener("click", () => {
+    let cookiesConsentBanner = document.querySelector("div.cookie-consent-banner");
+    cookiesConsentBanner.remove();
+    ["header", "main", "footer"].forEach(element => {
+        document.getElementsByTagName(element)[0].classList.remove("blur");
+    })
+});
