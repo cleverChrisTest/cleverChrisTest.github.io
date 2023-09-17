0454dce3cf94b5239c506e0798d66031f7f58014
Christian <82599146+cleverChrisTest@users.noreply.github.com>
2023-09-17 18:13:21 +0100

    Update

--- js/banner.js
+++ js/banner.js
@@ -1,7 +1,10 @@
+
 decline.addEventListener("click", () => {
     let cookiesConsentBanner = document.querySelector("div.cookie-consent-banner");
     cookiesConsentBanner.remove();
     ["header", "main", "footer"].forEach(element => {
         document.getElementsByTagName(element)[0].classList.remove("blur");
     })
+    
+
 });
